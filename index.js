2dee3a72484b350948c94f8d1fbc1cb4a816261f
Pepijn Schoen <pepijn@eliandor.com>
2015-01-06 10:54:05 +0100

    Make Azure ember-deploy build (but not work yet)

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+var AzureAdapter = require('./lib/index-adapter');
+
 module.exports = {
-  name: 'ember-deploy-azure'
+  name: 'ember-deploy-azure',
+  type: 'ember-deploy-addon',
+
+  adapters: {
+    index: {
+      'azure': AzureAdapter
+    },
+    assets: {
+      'azure': AzureAdapter
+    }
+  }
 };
